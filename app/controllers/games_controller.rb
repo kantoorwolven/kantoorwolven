4cdf951b054301fde435a220f949252e214fae98
Ivan Malykh <ivan@lesslines.com>
2014-11-08 10:28:15 +0100

    list games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,11 @@ class GamesController < ApplicationController
   skip_before_action :verify_authenticity_token, if: :json_request?
   respond_to :json
 
+  def index
+    @games = Game.all
+    respond_with @games
+  end
+
   def create
     @game = Game.create game_params
     respond_with @game
