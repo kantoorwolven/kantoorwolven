875fa0d8cb3a496f28f5e90aecbd66875a10ee49
albertjan <albertjan@curit.com>
2014-11-08 11:31:10 +0100

    add game tmpl

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 Sammy('#main', function() {
     
+    var full = location.protocol+'//'+location.hostname;
+    var port = (location.port ? ':'+location.port: '');
+    
     this.use('Handlebars', 'hb');
     
     this.get('/', function() {
@@ -16,15 +19,13 @@ Sammy('#main', function() {
         }, 1000);
     });
     
-//    // define a 'get' route that will be triggered at '#/path'
-//    this.get('#/', function() {
-//          this.title = 'Hello!';
-//          this.name = this.params.name;
-//          // render the template and pass it through handlebars
-//          this.partial('templates/home.hb');
-//    });
-    
     this.get("#/pick_game", function () {
-        
+        var zis = this;
+    
+        $.getJSON(full + ":3000" + "/games.json", function(games){
+            zis.games = games;
+            
+            this.partial('templates/pick_games.hb');
+        });
     });
 }).run();
---
+++ app/templates/pick_game.hb
@@ -0,0 +1,6 @@
+<ul>
+{{#each games}}
+    <li>{{name}}</li>
+{{/each}}
+</ul>
+<a class="pure-button" href="#/create_game">Create a game!</a>
