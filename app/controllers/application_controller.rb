e1778824b6ef34b35848e6ecdb6ad34fb69e13e4
Ivan Malykh <ivan@lesslines.com>
2014-11-09 12:49:45 +0100

    remove comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,26 +36,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # def build_rounds
-  #   day   = Chronic.parse('today 16:30')
-  #   night = Chronic.parse('today 22:00')
-  #
-  #   Game.all.each do |game|
-  #     players = game.players.size
-  #     # next if Time.zone.now < game.starttime
-  #     next if players < 8
-  #     next if game.rounds.any?
-  #     rounds = players - 2
-  #     rest = rounds.modulo(2)
-  #     rounds = (rounds - rest) / 2
-  #     rounds.times do |i|
-  #       game.rounds.create deadline: night + i.days, type: 'Night', active: i == 0
-  #       game.rounds.create deadline: day + i.days,   type: 'Day',   active: i == 0
-  #       # day_number = day_number + 1 if i.odd?
-  #     end
-  #   end
-  # end
-
   def build_rounds
     Game.build_rounds
   end
