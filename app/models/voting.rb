2bca6245a3e3e3b26fc22f0ef8ffd571c0be7199
Ivan Malykh <ivan@lesslines.com>
2014-11-09 13:12:07 +0100

    update validation

--- app/models/voting.rb
+++ app/models/voting.rb
@@ -5,5 +5,5 @@ class Voting < ActiveRecord::Base
   belongs_to :voted, class_name: 'Player', foreign_key: :voted_id
 
   # Validations
-  validates :voter_id, uniqueness: {scope: [:round_id]}
+  validates :voter_id, uniqueness: {scope: [:round_id, :voted_id]}
 end
